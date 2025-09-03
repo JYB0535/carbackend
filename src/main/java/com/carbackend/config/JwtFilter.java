e35f3f69105fe564327aa56e785a9bf4a700f8c0
JYB0535 <jnk8528@gmail.com>
2025-09-03 12:35:06 +0900

    2025-09-03
    2025-09-03

--- src/main/java/com/carbackend/config/JwtFilter.java
+++ src/main/java/com/carbackend/config/JwtFilter.java
@@ -31,12 +31,15 @@ public class JwtFilter extends OncePerRequestFilter {
             //2. 꺼낸 토큰에서 유저 정보를 추출
             String username = jwtService.parseToken(request);
 
-            //3. 추출된 유저 정보로 Authentication을 만들어서 SecurityContext 에 set
-            Authentication authentication = new UsernamePasswordAuthenticationToken(
-                    username,
-                    null, //패스워드는 안 들어가도 되기때문에 널이다?
-                    Collections.emptyList()); //빈 리스트 원래는 권한 자리인데 비우고 만들어준다
-            SecurityContextHolder.getContext().setAuthentication(authentication);  //셋 해줌
+            //3. 추출된 유저 정보로 Authentication을 만들어서 SecurityContext 에 set (토큰 제거하고 나면 유저정보가 없음에도 정상처리를 하여서 수정을 조금 함)
+            //토큰이 없는 요청이 오면 parse토큰하면 유저네임이 비어있음
+            if(username != null) { //유저 정보가 정상적으로 파싱되어야 인증정보 담게 하는것 
+                Authentication authentication = new UsernamePasswordAuthenticationToken(
+                        username,
+                        null, //패스워드는 안 들어가도 되기때문에 널이다?
+                        Collections.emptyList()); //빈 리스트 원래는 권한 자리인데 비우고 만들어준다
+                SecurityContextHolder.getContext().setAuthentication(authentication);  //셋 해줌
+            }
         }
 
         // 마지막에 다음 필터를 호출
